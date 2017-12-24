09389d8627c7211015be5c16f15001e62367a0bb
Yardenng <yarden.nagel\@mail.huji.ac.il>
2017-12-24 02:39:47 +0200

    adding JackTokenizer

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -1,14 +1,16 @@
 import re
-# CONSTANTS
-KEYWORD = 'keyword'
-SYMBOL = 'symbol'
-STRING = 'stringConstant'
-INTEGER = 'integerConstant'
-IDENTIFIER = 'identifier'
 
-# my tokenizer  VV
+# my tokenizer
 
 class JackTokenizer:
+    # CONSTANTS
+    KEYWORD = 'keyword'
+    SYMBOL = 'symbol'
+    STRING = 'stringConstant'
+    INTEGER = 'integerConstant'
+    IDENTIFIER = 'identifier'
+
+    symbol_dict = {"<": "&lt;", ">": "&gt;", "&": "&amp;"}
 
     INTEGER_REGEX = '\d+'
     STRING_REGEX = '"[^"\n]*."'
@@ -17,6 +19,10 @@ class JackTokenizer:
     WORD_REGEX = '((class)|(constructor)|(function)|(method)|(field)|(static)' \
                  '|(var)|(int)|(char)|(boolean)|(void)|(true)|(false)|(null)' \
                  '|this|let|do|if|else|while|return))'
+    LEXICAL_ANALYSIS = re.compile('{}|{}|{}|{}|{}'.format(WORD_REGEX,
+                                      SYMBOL_REGEX, INTEGER_REGEX,
+                                           STRING_REGEX, IDENTIFIER_REGEX))
+
 
     "Opens the input file/stream and gets ready to tokenize it."
     def ___init___(self, inputFile):
@@ -40,82 +46,44 @@ class JackTokenizer:
 
     def remove_comments(self):
         currentIndex = 0
-        filteredText = ''
+        text_with_no_comments = ''
         while currentIndex < len(self.lines):
             currentChar = self.lines[currentIndex]
             if currentChar == "\"":
                 endIndex = self.lines.find("\"", currentIndex + 1)
-                filteredText += self.lines[currentIndex:endIndex + 1]
+                text_with_no_comments += self.lines[currentIndex:endIndex + 1]
                 currentIndex = endIndex + 1
             elif currentChar == "/":
                 if self.lines[currentIndex + 1] == "/":
                     endIndex = self.lines.find("\n", currentIndex + 1)
                     currentIndex = endIndex + 1
-                    filteredText += " "
+                    text_with_no_comments += " "
                 elif self.lines[currentIndex + 1] == "*":
                     endIndex = self.lines.find("*/", currentIndex + 1)
                     currentIndex = endIndex + 2
-                    filteredText += " "
+                    text_with_no_comments += " "
                 else:
-                    filteredText += self.lines[currentIndex]
+                    text_with_no_comments += self.lines[currentIndex]
                     currentIndex += 1
             else:
-                filteredText += self.lines[currentIndex]
+                text_with_no_comments += self.lines[currentIndex]
                 currentIndex += 1
-        self.lines = filteredText
-        return
+        self.lines = text_with_no_comments
 
-        # re.sub(re.compile("\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+"), "",
-        #        lines)
-        # finder = re.findall(re.compile("\"[^\n\"]*\/\/[^\n\"]*\""), lines)
-        # splittedContent = re.split(re.compile("\"[^\n\"]*\/\/[^\n\"]*\""), lines)
-        # text_with_no_comments = re.sub(re.compile("\/\/[^\n]*\n"), "\n", splittedContent[0])
-        #
-        # # text_with_no_comments = ""
-        # for char in self.lines:
-        #     cur = self.lines[index]
-        #     if cur == "\"":
-        #         end = self.lines.find("\"", index+1)
-        #         text_with_no_comments += self.lines[index:end]
-        #     elif cur == "/":
-        #         if self.lines[index +1]== "/":
-        #             end = self.lines.find("\n", index +1)
-        #             text_with_no_comments +=""
-        #         elif self.lines[index +1]== "*":
-        #             end = self.lines.find("*/", index+1)
-        #             text_with_no_comments+=""
-        #         else:
-        #             text_with_no_comments += self.lines[index]
-        #     else:
-        #         text_with_no_comments += self.lines[index]
-        # self.lines = text_with_no_comments
-        # return self.lines
 
     def tokenizer(self):
-        filter_lines = self.split(self.lines)
+        filter_lines = self.LEXICAL_ANALYSIS.findall(self.lines)
         for word in filter_lines:
             self.tokens.append(word)
         return self.tokens
-        # for lines in self.lines:
-        #     self.remove_comments()
 
-    # def remove_multiLine(self, content):
-    #     return re.sub(re.compile("\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\/"), "", content)
-
-    # def remove_singleLines(self, content):
-    #     finder = re.findall(re.compile("\"[^\n\"]*\/\/[^\n\"]*\""), content)
-    #     splittedContent = re.split( re.compile("\"[^\n\"]*\/\/[^\n\"]*\""), content)
-    #     newContent = re.sub(False, "\n", splittedContent[0])
-    #     for idx, elem in enumerate(splittedContent[1:]):
-    #         newContent += finder[idx] + "\n" + re.sub(False, "\n",
-    #                                                   elem)
-    #     return newContent
     def hasMoreTokens(self):
         """
         Do we have more tokens in the input?
         :return: boolean
         """
         return len(self.tokens) != 0
+
 
     def advance(self):
         """
@@ -126,10 +94,7 @@ class JackTokenizer:
         :return:
         """
         if self.hasMoreTokens():
-            self.tokenData =  self.tokens.pop(0)
-
-
-
+            self.tokenData = self.tokens.pop(0)
 
     def tokenVal(self):
         return self.tokenData[1]
